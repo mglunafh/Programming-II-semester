c433bf06e71b7c4f6b624ec86472067c97f2fa32
mglunafh <ufispb@gmail.com>
2012-05-23 18:36:38 +0400

    Interface of stack, abstract stack, and stack as linked list.

---
+++ 2012.03.02_IV/Calculator/src/stack/EmptyStackException.java
@@ -0,0 +1,9 @@
+package stack;
+
+/**
+ *
+ * @author Dev
+ */
+public class EmptyStackException extends Exception {
+    
+}
---
+++ 2012.03.02_IV/Calculator/src/stack/ListStack.java
@@ -0,0 +1,64 @@
+package stack;
+
+/**
+ *
+ * @author Dev
+ */
+public class ListStack {
+
+    ListStack() {
+        this.head = new StackElement(0, null);
+    }
+
+    /**
+     *
+     * @return true in case of empty stack
+     */
+    public boolean isEmpty() {
+        return (null == this.head.next);
+    }
+
+    /**
+     *
+     * @param value which is been put into the stack.
+     */
+    public void push(int value) {
+        StackElement newHead = new StackElement(value, this.head);
+        this.head = newHead;
+    }
+
+    /**
+     *
+     * @throws EmptyStackException raises, when stack is empty.
+     */
+    public void pop() throws EmptyStackException {
+        if (this.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        this.head = this.head.next;
+    }
+
+    /**
+     *
+     * @return a value on top of the stack.
+     * @throws EmptyStackException raises, when method tries to put a value from
+     * empty stack.
+     */
+    public int top() throws EmptyStackException {
+        if (this.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return this.head.value;
+    }
+
+    private class StackElement {
+
+        public StackElement(int value, StackElement next) {
+            this.value = value;
+            this.next = next;
+        }
+        private int value;
+        private StackElement next;
+    }
+    private StackElement head;
+}
