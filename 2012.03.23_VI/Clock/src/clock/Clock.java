cec81cc4d743f198bda822e2f2feac56b834f986
mglunafh <ufispb@gmail.com>
2012-06-01 07:36:18 +0400

    Clock, for example.

---
+++ 2012.03.23_VI/Clock/src/clock/Clock.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clock;
+
+/**
+ *
+ * @author Dev
+ */
+public class Clock {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
